1132a0e23b07ba8d2942ce566be3675d55c18249
Mattias Bengtsson <mattias.jc.bengtsson@gmail.com>
2026-07-15 06:15:20 +0200

    Caddy: Expose --config and --adapter as env vars

--- caddy/rootfs/usr/local/bin/caddy.sh
+++ caddy/rootfs/usr/local/bin/caddy.sh
@@ -2,6 +2,14 @@
 
 set -eu
 
+case "${1}" in
+    reload|run|start|stop|validate)
+        set -- "${@}"                                                          \
+            ${CADDYSH_CONFIG:+--config="${CADDYSH_CONFIG}"}                    \
+            ${CADDYSH_CONFIG_ADAPTER:+--adapter="${CADDYSH_CONFIG_ADAPTER}"}  ;;
+    *) ;;
+esac
+
 if [ -z "${CADDYSH_LOG_JQ:-}" ] || [ "${CADDYSH_LOG_JQ:-}" = "0" ]; then
     exec /usr/bin/caddy "${@}"
 fi
